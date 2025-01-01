4e1485c8261cce3d3693fae50fbe44f7781d8b24
boyplus <ict224bj@gmail.com>
2025-01-01 23:26:01 +0700

    update: Maps

--- src/main/scala/lecture/part3/TuplesAndMaps.scala
+++ src/main/scala/lecture/part3/TuplesAndMaps.scala
@@ -12,11 +12,38 @@ object TuplesAndMaps extends App {
   val aMap: Map[String, Int] = Map() //  call apply method
 
   // each element is tuple
-  val phonebook = Map(("Jim", 555), "Daniel" -> 789)
+  val phonebook = Map(("Jim", 555), "Daniel" -> 789).withDefaultValue(-1)
   // a-> b is sugar for (a, b)
 
   // map ops
   println(phonebook.contains("Jim"))
   println(phonebook("Jim"))
   println(phonebook("Mary")) // Map.apply with the key that does not exist will throw an exception
+
+  // add a pairing
+  val newPairing = "Mary" -> 678
+  val newPhonebook = phonebook + newPairing // because Map is immutable
+  println(newPhonebook)
+
+  // functions on maps
+  // map, flatMap, filter
+
+  println(phonebook.map(pair => pair._1.toLowerCase -> pair._2))
+
+  // filter keys
+  println(phonebook.view.filterKeys(x => x.startsWith("J")).toMap)
+
+  // mapValues
+  println(phonebook.view.mapValues(number => "0245-" + number).toMap)
+
+
+  // conversion to other collections
+  println(phonebook.toList)
+  println(List(("Daniel", 555)).toMap)
+
+
+  val names = List("Bob", "James", "Angela", "Mary", "Daniel", "Jim")
+  println(names.groupBy(name => name.charAt(0)))
+
+  // Takeaways
 }
