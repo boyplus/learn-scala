dea4aabb605ded4e9ad51146c599d146634c0dee
boyplus <ict224bj@gmail.com>
2025-01-05 14:23:51 +0700

    update: handling failure

--- src/main/scala/lecture/part3/HandlingFailure.scala
+++ src/main/scala/lecture/part3/HandlingFailure.scala
@@ -1,6 +1,6 @@
 package lecture.part3
 
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Random, Success, Try}
 
 object HandlingFailure extends App {
   // create success and failure
@@ -20,7 +20,42 @@ object HandlingFailure extends App {
   val anotherPotentialFailure = Try {
     // code that might throw
   }
-  
+
   // utilities
-  
+  println(potentialFailure.isSuccess)
+  println(potentialFailure.isFailure)
+
+  // orElse
+  def backupMethod(): String = "A valid result"
+  val fallbackTry = Try(unsafeMethod()).orElse(Try(backupMethod()))
+  println(fallbackTry)
+
+  // If you design the API
+  def betterUnsafeMethod(): Try[String] = Failure(new RuntimeException())
+  def betterBackupMethod(): Try[String] = Success("A valid result")
+  val betterFallback = betterUnsafeMethod().orElse(betterBackupMethod())
+  println(betterFallback)
+
+
+  // map, flatMap, filter
+  println(aSuccess.map(_ * 2))
+  println(aSuccess.flatMap(x => Success(x * 10)))
+  println(aSuccess.filter(_ > 10)) // This will be Failure object with NoSuchElementException
+
+  // => for-comprehensions
+
+  /*
+    Exercise
+   */
+
+  val hostname = "localhost"
+  val port = "8080"
+  def renderHTML(page: String) = println(page)
+
+  class Connection{
+    def get(url: String): String = {
+      val random = new Random(System.nanoTime())
+      ""
+    }
+  }
 }
